ed0da3a5619516eae4772b9021788039497ab798
brianbartilet <brian.bartilet@gmail.com>
2024-03-07 13:58:36 +0800

    brian-bartilet: complete basic config loader

--- utilities/config/tests/unit_tests.py
+++ utilities/config/tests/unit_tests.py
@@ -7,12 +7,14 @@ class TestsUnitConfigLoader(unittest.TestCase):
     def test_without_keywords(self):
         loader = ConfigLoader()
         data = loader.load()
-        assert(isinstance(data, dict), True)
+        assert (isinstance(data, dict), True)
+
     def test_override_path(self):
         path = os.path.join(os.getcwd(), 'location')
         loader = ConfigLoader(base_path=path)
         data = loader.load()
         assert (isinstance(data, dict), True)
+
     def test_override_path_yaml(self):
         file = 'data.yaml'
         loader = ConfigLoader(name=file)
