b8c2134ff87c94fcba9c7ab9f9c4872ffc715016
brianbartilet <brian.bartilet@gmail.com>
2024-03-20 01:22:50 +0800

    added packages setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,20 @@ with open('requirements.txt') as f:
 
 setup(
     name='harqis_core',
-    version='1.0.0',
+    version="0.1.0",
     packages=find_packages(),
+    author="Brian Bartilet",
+    author_email="brian.bartilet@gmail.com",
     install_requires=requirements,
+    description="Heuristic Automation for a Reliable Quality Integration System",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/brianbartilet/harqis-core",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    homepage="https://github.com/brianbartilet/harqis-core",
 )
