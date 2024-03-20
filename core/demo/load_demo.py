8d5f52bc4a0c3d4761fa879b3c3d7d778a806b0c
brianbartilet <brian.bartilet@gmail.com>
2024-03-21 01:50:23 +0800

    chore: update demo

--- core/demo/load_demo.py
+++ core/demo/load_demo.py
@@ -4,20 +4,8 @@ import yaml
 
 # RUN THIS ONLY IN DEMO PROJECT https://github.com/brianbartilet/harqis-demo-testing
 
-def copy_directory(source_dir, target_dir):
-    try:
-        # Check if the target directory already exists
-        if os.path.exists(target_dir):
-            # If it exists, remove it to avoid errors
-            shutil.rmtree(target_dir)
-        # Copy the source directory to the target directory
-        shutil.copytree(source_dir, target_dir)
-        print(f"Directory {source_dir} copied to {target_dir} successfully.")
-    except Exception as e:
-        print(f"Error copying directory: {e}")
 
-
-def create_and_move_yaml(filename, data: dict, destination):
+def create_and_move_yaml(filename: str, data: dict, destination):
     # Create the YAML file with content
     with open(filename, 'w') as file:
         yaml.dump(data, file)
@@ -27,18 +15,7 @@ def create_and_move_yaml(filename, data: dict, destination):
     print(f"{filename} has been moved to {destination}")
 
 
-if __name__ == "__main__":
-    # Specify the source and target directories
-    source_directory = os.path.join(os.getcwd(), "venv",
-                                                 "lib",
-                                                 "site-packages",
-                                                 "core",
-                                                 "demo")
-
-    target_directory = os.path.join(os.getcwd(), "demo")
-
-    # Call the function to copy the directory
-    copy_directory(source_directory, target_directory)
+def create_demo_configurations():
 
     # Create the config.yaml file for the __template_tests_services_graphql
     config_template_rest = {
@@ -52,7 +29,10 @@ if __name__ == "__main__":
         }
     }
     create_and_move_yaml("config.yaml", config_template_rest,
-                         os.path.join(os.getcwd(), "__template_tests_services_rest"))
+                         os.path.join(os.getcwd(),
+                                      "demo",
+                                      "testing",
+                                      "__template_tests_services_rest"))
 
     # Create the config.yaml file for the __template_tests_services_graphql
     config_template_graph = {
@@ -66,4 +46,7 @@ if __name__ == "__main__":
         }
     }
     create_and_move_yaml("config.yaml", config_template_rest,
-                         os.path.join(os.getcwd(), "__template_tests_services_graphql"))
+                         os.path.join(os.getcwd(),
+                                      "demo",
+                                      "testing",
+                                      "__template_tests_services_graphql"))
