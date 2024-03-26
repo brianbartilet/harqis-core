a4240f57db85bd36a4b710aaca62bf87c148b5a9
brianbartilet <brian.bartilet@gmail.com>
2024-03-27 00:42:26 +0800

    chore: move demo tests to pytest

--- core/demo/testing/__tpl_tests_services_graphql/tests/sanity/mutations/save_media_list_entry.py
+++ core/demo/testing/__tpl_tests_services_graphql/tests/sanity/mutations/save_media_list_entry.py
@@ -1,21 +1,24 @@
-import unittest
-
+import pytest
 from http import HTTPStatus
 
 from demo.testing.__tpl_tests_services_graphql.config import CONFIG
-from demo.testing.__tpl_tests_services_graphql.mutations.save_media_list_entry.mutation \
-    import ServiceMutationSaveMediaListEntry
+from demo.testing.__tpl_tests_services_graphql.mutations.save_media_list_entry.mutation import ServiceMutationSaveMediaListEntry
 
 
-class TestsUnitWebServices(unittest.TestCase):
-    def setUp(self):
-        self.given_mutation = ServiceMutationSaveMediaListEntry(CONFIG)
-        self.given_payload = {'id': 26, 'mediaId': 8, 'status': "REPEATING" }
+@pytest.fixture()
+def set_up():
+    given_mutation = ServiceMutationSaveMediaListEntry(CONFIG)
+    given_payload = {'id': 26, 'mediaId': 8, 'status': "REPEATING"}
 
-    def test_mutation_save_media_with_test_case_data(self):
-        when_response = self.given_mutation.test_mutation_save_media_with_test_case_data(self.given_payload)
-        then = self.given_mutation.verify.common
-        then.assert_that(when_response.status_code, then.equal_to(HTTPStatus.BAD_REQUEST))
+    return given_mutation, given_payload
 
-        then.assert_that(when_response.data, then.has_key('SaveMediaListEntry'))
-        self.assertTrue(when_response.data['SaveMediaListEntry'] is None)
+
+def test_mutation_save_media_with_test_case_data(set_up):
+    given_mutation, given_payload = set_up
+
+    when_response = given_mutation.test_mutation_save_media_with_test_case_data(given_payload)
+    then = given_mutation.verify.common
+    then.assert_that(when_response.status_code, then.equal_to(HTTPStatus.BAD_REQUEST))
+
+    then.assert_that(when_response.data, then.has_key('SaveMediaListEntry'))
+    then.assert_that(True, when_response.data['SaveMediaListEntry'] is None)
--- core/demo/testing/__tpl_tests_services_rest/tests/sanity/post.py
+++ core/demo/testing/__tpl_tests_services_rest/tests/sanity/post.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 
 from http import HTTPStatus
 
@@ -8,34 +8,41 @@ from demo.testing.__tpl_tests_services_rest.dto.user import DtoUser
 from demo.testing.__tpl_tests_services_rest.dto.payload import DtoPostPayload
 
 
-class TestsUnitWebServices(unittest.TestCase):
-    def setUp(self):
-        self.given = ServiceRestExamplePost(CONFIG)
+@pytest.fixture()
+def given():
+    given = ServiceRestExamplePost(CONFIG)
 
-        self.post_payload = {
-            'title': 'Test Post',
-            'body': 'This is a test post.',
-            'userId': 1
-        }
+    given_payload = {
+        'title': 'Test Post',
+        'body': 'This is a test post.',
+        'userId': 1
+    }
+    return given, given_payload
 
-    def test_run_unit_tests_get(self):
-        when = self.given.request_get()
-        then = self.given.verify.common
-        then.assert_that(when.status_code, then.equal_to(HTTPStatus.OK))
-        then.assert_that(isinstance(when.data, DtoUser), True)
 
-    def test_run_unit_tests_post(self):
-        given_payload = DtoPostPayload(**self.post_payload)
-        when = self.given.request_post(given_payload)
-        then = self.given.verify.common
-        then.assert_that(when.status_code, then.equal_to(HTTPStatus.CREATED))
-        then.assert_that(when.data, then.has_entries())
+def test_run_unit_tests_get(given):
+    given, given_payload = given
+    when = given.request_get()
+    then = given.verify.common
+    then.assert_that(when.status_code, then.equal_to(HTTPStatus.OK))
+    then.assert_that(isinstance(when.data, DtoUser), True)
 
-    def test_run_unit_tests_delete(self):
-        given_request = self.given.request_delete()
-        when = self.given.send_request(given_request)
-        then = self.given.verify.common
-        then.assert_that(when.status_code, then.equal_to(HTTPStatus.NOT_FOUND))
+
+def test_run_unit_tests_post(given):
+    given, given_payload = given
+    given_payload_ = DtoPostPayload(**given_payload)
+    when = given.request_post(given_payload_)
+    then = given.verify.common
+    then.assert_that(when.status_code, then.equal_to(HTTPStatus.CREATED))
+    then.assert_that(when.data, then.has_entries())
+
+
+def test_run_unit_tests_delete(given):
+    given, given_payload = given
+    given_request = given.request_delete()
+    when = given.send_request(given_request)
+    then = given.verify.common
+    then.assert_that(when.status_code, then.equal_to(HTTPStatus.NOT_FOUND))
 
 
 
