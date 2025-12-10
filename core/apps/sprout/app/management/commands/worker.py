95e186c894a379f93728031288a1f0992be8a848
brianbartilet <brian.bartilet@gmail.com>
2025-12-10 23:11:27 +0800

    (work) break down worker queues

--- core/apps/sprout/app/management/commands/worker.py
+++ core/apps/sprout/app/management/commands/worker.py
@@ -7,14 +7,16 @@ from core.apps.sprout.settings import APP_PACKAGE
 from core.config.env_variables import ENV_WORKFLOW_CONFIG
 from core.config.env_variables import ENV_WORKFLOW_CONCURRENCY
 from core.config.env_variables import ENV_WORKFLOW_QUEUE
+print('====================================================================================')
+print(f'{APP_PACKAGE}')
+print(f'{ENV_WORKFLOW_CONFIG}')
+print(f'{ENV_WORKFLOW_CONCURRENCY}')
+print(f'{ENV_WORKFLOW_QUEUE}')
+print('====================================================================================')
 
 
 def restart_celery_worker_tasks():
     """Restart the Celery scheduler for the workflow app."""
-    print(f'{APP_PACKAGE}')
-    print(f'{ENV_WORKFLOW_CONFIG}')
-    print(f'{ENV_WORKFLOW_CONCURRENCY}')
-    print(f'{ENV_WORKFLOW_QUEUE}')
     restart_celery_worker(APP_PACKAGE, ENV_WORKFLOW_CONFIG,
                           concurrency=ENV_WORKFLOW_CONCURRENCY,
                           queue=ENV_WORKFLOW_QUEUE
